ef6982f7abd7cae6efbdc3091952b9daa250da2c
Mai Hieu <maiminhhieu1999@gmail.com>
2022-05-18 12:54:57 +0700

    entity

--- src/main/java/com/example/demologwork/dto/LogWorkDto.java
+++ src/main/java/com/example/demologwork/dto/LogWorkDto.java
@@ -1,36 +1,12 @@
 package com.example.demologwork.dto;
 
-<<<<<<< HEAD
-import com.example.demologwork.entity.LogWorkEntity;
-=======
-import com.example.demologwork.model.Request;
-import com.example.demologwork.model.User;
->>>>>>> 2a1d0a17504a62678809110d73cda4ee4eae45eb
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Time;
-<<<<<<< HEAD
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class LogWorkDto {
-    private Long id;
-    private Integer day;
-    private Integer month;
-    private Integer year;
-    private Time startTime;
-    private Time endTime;
-    private Time workTime;
-    private Time overTime;
-    public static LogWorkDto EntityToDto(LogWorkEntity logWorkEntity){
-
-    }
-=======
 import java.util.List;
 
 @NoArgsConstructor
@@ -48,5 +24,4 @@ public class LogWorkDto {
     private String overTime;
     private UserDto userDto;
     private List<RequestDto> requestList;
->>>>>>> 2a1d0a17504a62678809110d73cda4ee4eae45eb
 }
