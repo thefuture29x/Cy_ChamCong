d6b74d3ae703ae1936981776b2349aef9ad61610
tranhoangmanh <48063081+tranhoangmanh@users.noreply.github.com>
2022-05-18 10:06:40 +0700

    Init project

---
+++ src/main/java/com/example/logwork_hackintosh/LogworkHackintoshApplication.java
@@ -0,0 +1,13 @@
+package com.example.logwork_hackintosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogworkHackintoshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogworkHackintoshApplication.class, args);
+    }
+
+}
