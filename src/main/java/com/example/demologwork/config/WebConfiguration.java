937fba1e0810134b56e61d652ad9a9de0cf57dfd
Seabreeze <teothiphong@gmail.com>
2022-05-19 15:43:07 +0700

    test

--- src/main/java/com/example/demologwork/config/WebConfiguration.java
+++ src/main/java/com/example/demologwork/config/WebConfiguration.java
@@ -23,7 +23,7 @@ public class WebConfiguration implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOriginPatterns("*")
-                        .allowCredentials(true)
+                        .allowCredentials(false)
                         .allowedMethods("*");
             }
         };
